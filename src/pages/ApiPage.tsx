48d93126205ccadb922307d2e62a86c3655884a3
Hanseok Lim <hslim@pivotbridge.com>
2026-07-10 14:14:20 +0900

    url 수정

--- src/pages/ApiPage.tsx
+++ src/pages/ApiPage.tsx
@@ -7,7 +7,7 @@ interface MessageResponse {
 
 function ApiPage(){
   const [data, setData] = useState<string>("로딩 중...");
-  const BASE_URL = 'http://localhost:8000'
+  const BASE_URL = ''
   useEffect(()=>{
     fetch(`${BASE_URL}/api/message`)
     .then((res) => res.json())
